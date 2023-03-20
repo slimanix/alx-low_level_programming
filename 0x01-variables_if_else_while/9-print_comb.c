57ab84c36f8b7ff8df01a6cc300bf87779e2676e
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-20 16:34:10 -0700

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - Entry poin
+ *
+ *Description: print 0, 1, -9
+ *
+ *Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+    int digit = 0;
+    while (digit <= 9)
+    {
+                 putchar(digit + 48);
+                 if (digit != 9)
+                 {
+                           putchar(',');
+                           putchar(' ');
+                 }
+                 digit++;
+    }
+    putchar('\n');
+    return (0);
+}
