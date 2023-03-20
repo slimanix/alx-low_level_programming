f1ccc8ab12ea43c7ec2715dba368b50cf5df5958
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-20 13:05:10 -0700

    commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-int main()
+/**
+*main-Entry point
+*
+*Decription: print 0-9 using putchar while using variable
+*
+*
+*Return : Always 0 (Success)
+*/
+
+int main(void)
 {
-    int n;
-    n = 48;
-    while(n<=57)
+    int n = 0;
+    while (n <= 9)
     {
-                 putchar(n);
+                 putchar(n + '0');
                  n++;
     }
     putchar('\n');
-    
+    return(0);
 }
