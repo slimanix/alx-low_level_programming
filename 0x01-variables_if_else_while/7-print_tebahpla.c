c973c1b0fe2309da1b3be609c503dfa030be9d09
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-20 17:41:18 -0700

    commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
- *main - Entry poin
- *
- *Description: print a-z in reverse
- *
- *Return: Always 0 (Sucess)
+*main - Entry poin
+*
+*Description: print a-z in reverse
+*
+*Return: Always 0 (Sucess)
 */
 
 int main(void)
 {
-    char n = 'z';
-    while (n >= 'a')
-    {
-                 putchar(n);
-                 n--;
-    }
-    putchar('\n');
-    return (0);
+char n = 'z';
+while (n >= 'a')
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@
 
 int main(void)
 {
-    int digit = 48;
-    while (digit <= 102)
-    {
-                 putchar(digit);
-                 if (digit == 57)
-                    digit += 39;
-                 digit++;
-    }
-    putchar('\n');
-    return (0);
+int digit = 48;
+while (digit <= 102)
+{
+putchar(digit);
+if (digit == 57)
+digit += 39;
+digit++;
+}
+putchar('\n');
+return (0);
 }
