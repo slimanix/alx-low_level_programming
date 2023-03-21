39ba5e22a426ea810cf0700fd32eac63dce1f7ca
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-20 17:44:47 -0700

    commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-    int n = 0;
-    while (n <= 9)
-    {
-                 putchar(n + '0');
-                 n++;
-    }
-    putchar('\n');
-    return(0);
+int n = 0;
+while (n <= 9)
+{
+putchar(n + '0');
+n++;
+}
+putchar('\n');
+return (0);
 }
