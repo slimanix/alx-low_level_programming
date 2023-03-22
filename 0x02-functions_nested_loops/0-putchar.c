50ce328a825fd41e3386e2b314b7cfb8f15386b4
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-22 16:49:09 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,11 @@
 #include "main.h"
-/**
- *main -Entry point
- *
- * Description : prints _putchar using _putchar prototype
- *
- * Return : Always 0 Success
- * */
+/* main -Entry point */
+/* Description : prints _putchar using _putchar prototype*/
+/* Return : Always 0 Success */
 int main(void)
 {
-char str[] = "_putchar" ;
-int ch ;
+char str[] = "_putchar";
+int ch;
 for (ch = 0; ch < 8; ch++)
 {
 _putchar(str[ch]);
