7db7c705085af5688dd3cdfd8c04f545a4613774
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-23 12:53:39 -0700

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+* print_last_digit - print the last digit
+* @n: the character to be checked
+*
+* Return:the last digit
+*/
+
+int print_last_digit(int n)
+{
+int l;
+l = n % 10;
+if (l < 10)
+{
+return (-l);
+}
+else if
+{
+return (l);
+}
+}
