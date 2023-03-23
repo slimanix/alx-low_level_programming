6a3df09996fa90c47aeed7a7955b7ae48cddcd85
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-23 06:41:48 -0700

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+* print_sign - return + if n positve - if negative
+* 0 if n equal zero
+* @n: the character to be checked
+*
+* Return: 1 if n positive, -1 if n negative
+* 0 if n equal zero
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+_putchar('\n');
+}
