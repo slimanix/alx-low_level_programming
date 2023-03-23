b7304e31946fabb22b896a2cfca726020feaaf30
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-23 08:05:59 -0700

    commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,19 +2,19 @@
 
 /**
  * _abs - return absolut methode of a intiger
- * @int: the character to be checked
+ * @n: the character to be checked
  *
  * Return: Absolute value
  */
 
 int _abs(int)
 {
-if (int >= 0)
+if (n >= 0)
 {
-return (int);
+return (n);
 }
 else
 {
-return (int * -1);
+return (n * -1);
 }
 }
