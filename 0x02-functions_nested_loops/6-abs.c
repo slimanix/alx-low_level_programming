b95a41f2daa9b059cc3ffc307555b52aa168b5c6
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-23 08:01:43 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * _abs - return absolut methode of a intiger
+ * @i: the character to be checked
+ *
+ * Return: Absolute value
+ */
+
+int _abs(i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (i * -1);
+}
+}
