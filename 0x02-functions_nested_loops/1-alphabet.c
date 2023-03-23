6b9464b6c219fb934faee68e7a7544050f45bb46
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-22 17:22:43 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
-/* print_alphabet - utulizes on the _putchar function */ 
+/* print_alphabet - utulizes on the _putchar function */
 
 /* to print the alphabet a - z */
-
 void print_alphabet(void)
 {
 int ch;
