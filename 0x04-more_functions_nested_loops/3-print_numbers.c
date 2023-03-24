6aa9fe0bfd01ebc6427c14c9f6065a46795ae879
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-23 19:22:11 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+* print_numbers - print 0 - 9
+*
+*
+* Return: Always 0 (Success)
+*/
+
+
+void print_numbers(void)
+{
+int num = 0;
+for (num >= 0; num <= 9; num++)
+{
+_putchar(num + 48);
+}
+_putchar('\n')i;
+}
