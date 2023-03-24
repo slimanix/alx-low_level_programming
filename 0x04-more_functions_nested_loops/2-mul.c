9aaffb7b105529d9f2786c5b0d4151dee5ff8a82
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-23 19:10:14 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+* mul - return the multiplication
+* @a: the character to be checked
+* @b: the character to be checked
+*
+* Return: the multiplication of two digit
+*/
+
+
+int mul(int a, int b)
+{
+return (a * b);
+}
