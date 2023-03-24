753870dccdd4bdefdf702b8b47ed7170c303f48f
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-24 09:22:50 -0700

    commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,7 +12,7 @@ void print_square(int size)
 int row, column;
 for (row = 0; row <= size; row++)
 {
-for (column = 0; column <= size; column)
+for (column = 0; column <= size; column++)
 {
 _putchar('#');
 }
