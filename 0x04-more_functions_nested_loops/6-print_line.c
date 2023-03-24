8e585c815a6c4781e66a9f963d5300d5eb920567
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-03-24 05:00:12 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_line - print a straight line
+ * @n: the numer of straight lines
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_line(int n)
+{
+int a;
+if (a <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 1; a <= n; a++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
