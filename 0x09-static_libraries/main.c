70cff3e63881b221682d9bb0859642299d63b7f5
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-04-10 16:11:59 -0700

    commit

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -4,10 +4,8 @@
  * Return: length
  */
 
-
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best
-hacker\"\n\t- Kevin Mitnick");
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
