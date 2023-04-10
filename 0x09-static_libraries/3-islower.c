391976f8e29f7000baa74a893e97206cdf53c5a8
slimanix <elidrissislimaniabderrahmane@gmail.com>
2023-04-10 15:24:49 -0700

    static libraries

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,14 +1,11 @@
-#include"main.h"
+#include "main.h"
+
 /**
-* _islowercase - function to check if
-*               charactere is lowercase
-* @c : check input of function
-* Return: Return 1 if 'c' is lowercase
-*           otherwise always 0 (Success)
-*/
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
 int _islower(int c)
 {
-if (c >= 97 && c <= 122)
-return (1);
-return (0);
+	return (c >= 'a' && c <= 'z');
 }
